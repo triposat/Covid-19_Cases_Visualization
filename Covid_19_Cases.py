84054ce6bd28911beb97e1eee124c51cfe5d9a05
SATYAM TRIPATHI <satyamtripathi5554@gmail.com>
2021-05-01 16:25:00 +0530

    Update Covid_19_Cases.py

--- Covid_19_Cases.py
+++ Covid_19_Cases.py
@@ -3,29 +3,28 @@ import requests
 import json
 import datetime
 if __name__ == '__main__':
- while 1:
-    r=requests.get('https://coronavirus-19-api.herokuapp.com/countries').text.lower()
-    data=json.loads(r)
-    x=input("Please Enter The Name of the Country : ")
-    res = [sub for sub in data if sub['country']==x]
-    sa=res[0]['country']
-    x=data[0]['cases']
-    y=data[0]['deaths']
-    z=data[0]['recovered']
-    a=res[0]['cases']
-    b=res[0]['todaycases']
-    c=res[0]['deaths']
-    d=res[0]['todaydeaths']
-    e=res[0]['recovered']
-    dat=datetime.datetime.now().strftime("%d-%m-%y")
-    tim=datetime.datetime.now().strftime("%H : %M : %S")
-    notification.notify(title=f"  Covid-19 Live Updates\tDate : {dat}\tTime - {tim}",message=f"\n\t   WORLD "
-       f"\nConfirmed Cases ----> {x}\nDeaths ----> {y}\nRecovered ----> {z}\n\n\t    {sa.upper()}\nConfirmed Cases ----> "
-       f" {a}\nTotal Deaths ----> {c}\nTotal Recovered ----> {e}\nToday Cases ----> {b}\nToday Deaths ----> {d}"
-                           ,app_icon="C:/Users/Dell/Downloads/vir.ico",  timeout=10)
-    xyz=input("Do You Want to See More Records : Press[Y/N]").lower()
-    if xyz=='y':
-        continue
-    else:
-        exit()
-
+    while 1:
+        r = requests.get(
+            'https://coronavirus-19-api.herokuapp.com/countries').text.lower()
+        Data = json.loads(r)
+        x = input("Name of the Country: ")
+        Result = [sub for sub in Data if sub['country'] == x]
+        country = Result[0]['country']
+        cases = Data[0]['cases']
+        deaths = Data[0]['deaths']
+        recovered = Data[0]['recovered']
+        case = Result[0]['cases']
+        today_cases = Result[0]['todaycases']
+        death = Result[0]['deaths']
+        today_death = Result[0]['todaydeaths']
+        recover = Result[0]['recovered']
+        Today_date = datetime.datetime.now().strftime("%d-%m-%y")
+        Today_time = datetime.datetime.now().strftime("%H : %M : %S")
+        notification.notify(title=f"Covid-19 Live Updates\tDate : {Today_date}\tTime - {Today_time}", message=f"\n\t   WORLD "
+                            f"\nConfirmed Cases: {cases}\nDeaths: {deaths}\nRecovered: {recovered}\n\n\t    {country.upper()}\nConfirmed Cases: "
+                            f" {case}\nTotal Deaths: {death}\nTotal Recovered: {recover}\nToday Cases: {today_cases}\nToday Deaths: {today_death}", app_icon="D:\All IN ONE\Programming Notes\Icons/vir.ico", timeout=10)
+        Choice = input("Do You Want to See More Records: Press[Y/N]").lower()
+        if Choice == 'y':
+            continue
+        else:
+            exit()
